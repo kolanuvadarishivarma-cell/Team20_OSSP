30e6e5c13350f274eb961877195274a87417930a
charith2101 <charithk2101@gmail.com>
2026-08-11 09:04:26 +0530

    Add files via upload

---
+++ Skill/2520030356_Charith/Experiment1/exec_demo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+    printf("Before exec()\n");
+
+    execl("/bin/ls", "ls", "-l", NULL);
+
+    printf("This line will only print if exec() fails.\n");
+
+    return 0;
+}
